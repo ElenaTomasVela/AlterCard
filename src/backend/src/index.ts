ea645fdad2bbcbb368c5e96960b8c499e5559ea0
Elena Tomás Vela <eletomvel@alum.us.es>
2024-03-21 22:13:43 +0100

    feat: add docker-compose environment

--- src/backend/src/index.ts
+++ src/backend/src/index.ts
@@ -1,7 +1,16 @@
 import { Elysia } from "elysia";
+import mongoose from "mongoose";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+mongoose
+  .connect(
+    // `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}/${process.env.DB_NAME}`,
+    `mongodb://${process.env.DB_URL}/${process.env.DB_NAME}`,
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((e) => console.log(e));
+
+const app = new Elysia().get("/", () => "Hello Elysia!!!").listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
