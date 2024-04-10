af5a8f87a228b3fced200b6f8d93c77b8b67ed1e
Elena Tomás Vela <eletomvel@alum.us.es>
2024-04-10 14:06:34 +0200

    feat: add some text to main page

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { H1, H3 } from "./components/Headings";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <H1 className="text-primary-dark text-center">AlterUno</H1>
+      <H3 className="text-center">Your House, your Rules</H3>
+    </>
+  );
 }
 
 export default App;
---
+++ src/frontend/src/components/Headings.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const H1 = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return <h1 className={`font-bold text-3xl mb-2 ${className}`}>{children}</h1>;
+};
+
+export const H2 = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return <h2 className={`font-bold text-2xl ${className}`}>{children}</h2>;
+};
+
+export const H3 = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return <h3 className={`text-xl ${className}`}>{children}</h3>;
+};
