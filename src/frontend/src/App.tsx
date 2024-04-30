b9209f4d3fa4aade6d8ffa3ae7322c6784226306
Elena Tomás Vela <eletomvel@alum.us.es>
2024-04-30 18:55:06 +0200

    fix: tweak main page design

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,11 +1,35 @@
+import { Link } from "react-router-dom";
 import "./App.css";
-import { H1, H3 } from "./components/Headings";
+import { H1, H2, H3 } from "./components/Headings";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
-      <H1 className="text-primary-dark text-center">AlterUno</H1>
-      <H3 className="text-center">Your House, your Rules</H3>
+      <div className="flex justify-between flex-wrap gap-16 items-stretch">
+        <img
+          src="/undraw_group_hangout.svg"
+          className="mt-6 flex-shrink-0 flex-1"
+        />
+        <div className="flex flex-col items-center flex-1 gap-10 my-auto">
+          <span>
+            <h1 className="text-primary-dark text-7xl font-bold">AlterUno</h1>
+            <H3 className="text-center">Your House, your Rules</H3>
+          </span>
+          <div className="flex flex-col gap-3">
+            <span className="flex flex-center gap-7">
+              <Link to="/signup">
+                <Button className="px-7">Sign Up</Button>
+              </Link>
+              <Link to="/login">
+                <Button variant="outline" className="px-7">
+                  Log in
+                </Button>
+              </Link>
+            </span>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
