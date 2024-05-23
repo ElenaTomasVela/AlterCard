952537fd7e35517fb0d65f336ed812ec45259c69
Elena Tomás Vela <eletomvel@alum.us.es>
2024-05-24 01:45:08 +0200

    feat: add ongoing game listing

--- src/frontend/src/views/RoomListing.tsx
+++ src/frontend/src/views/RoomListing.tsx
@@ -1,4 +1,5 @@
-import { H1, H3 } from "@/components/Headings";
+import { H1, H2, H3 } from "@/components/Headings";
+import HightlightCard from "@/components/HightlightCard";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
@@ -14,32 +15,31 @@ const RoomCard = ({
 }) => {
   return (
     <Link to={`/rooms/${room._id}`} className="group focus:outline-none">
-      <Card
-        className="flex justify-between p-4 
-        group-hover:shadow-primary/10 group-hover:shadow-xl group-hover:border-primary/40
-        group-focus:shadow-primary/10 group-focus:shadow-xl group-focus:border-primary/40
-        group-focus:ring-2 group-focus:ring-primary/40
-        "
-      >
+      <HightlightCard className="flex justify-between p-4">
         <span>{room.host.username}'s Game</span>
         <span className="flex gap-1 items-center text-gray-500">
           <Icon icon="ic:round-person" className="size-6" />
           {room.users.length}
         </span>
-      </Card>
+      </HightlightCard>
     </Link>
   );
 };
 
 export function RoomListing() {
   const [rooms, setRooms] = useState([]);
+  const [games, setGames] = useState([]);
 
   const navigate = useNavigate();
 
   useEffect(() => {
-    const fetched = api.get("/room", { withCredentials: true });
-    fetched.then((res) => {
+    const fetchedRooms = api.get("/room");
+    const fetchedGames = api.get("/game");
+    fetchedRooms.then((res) => {
       setRooms(res.data);
+    });
+    fetchedGames.then((res) => {
+      setGames(res.data);
     });
   }, []);
 
@@ -50,26 +50,48 @@ export function RoomListing() {
 
   return (
     <>
-      <H1 className="mb-6">Join a Game</H1>
-      <div className="flex justify-between flex-wrap">
-        <Button className="md:w-1/3" onClick={createRoom}>
-          New Game
-        </Button>
-        <form className="flex lg:w-1/3">
-          <Input className="rounded-none rounded-l-lg border-2" />
-          <Button className="rounded-none rounded-r-lg">Join with code</Button>
-        </form>
+      <div className="flex flex-col gap-5">
+        <H1>Join a Game</H1>
+        <div className="flex justify-between flex-wrap">
+          <Button className="md:w-1/3" onClick={createRoom}>
+            New Game
+          </Button>
+          <form className="flex lg:w-1/3">
+            <Input className="rounded-none rounded-l-lg border-2" />
+            <Button className="rounded-none rounded-r-lg">
+              Join with code
+            </Button>
+          </form>
+        </div>
+        <div>
+          <H2>Your games in progress</H2>
+          <div className="grid gap-6 md:grid-cols-3 pt-5">
+            {games.map((game, index) => (
+              <Link
+                to={`/game/${game._id}`}
+                className="group focus:outline-none"
+              >
+                <HightlightCard className="flex justify-between p-4">
+                  Game {game._id}
+                </HightlightCard>
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div>
+          <H2>Open rooms</H2>
+          <div className="grid gap-6 md:grid-cols-3 pt-5">
+            {rooms.map((room, index) => (
+              <RoomCard key={index} room={room} />
+            ))}
+          </div>
+          {rooms.length == 0 && (
+            <H3 className="text-center w-full mt-5 text-gray-500">
+              There are no currently open rooms.
+            </H3>
+          )}
+        </div>
       </div>
-      <div className="grid gap-6 md:grid-cols-3 pt-5">
-        {rooms.map((room, index) => (
-          <RoomCard key={index} room={room} />
-        ))}
-      </div>
-      {rooms.length == 0 && (
-        <H3 className="text-center w-full mt-5 text-gray-500">
-          There are no currently open rooms.
-        </H3>
-      )}
     </>
   );
 }
