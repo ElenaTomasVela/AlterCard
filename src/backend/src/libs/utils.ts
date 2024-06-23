4a0712d8058c49cfc3df14d7b54b03ebaa443faf
Elena Tomás Vela <eletomvel@alum.us.es>
2024-06-23 17:12:51 +0200

    fix: remove unneeded function

--- src/backend/src/libs/utils.ts
+++ src/backend/src/libs/utils.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
-import { IPlayer } from "../models/game/schema";
 import { Card, CardColor, ICard } from "../models/card";
+import { IPlayer } from "../models/game/types";
 
 export function shuffle<T>(arr: T[]): T[] {
   // For shuffling, Fisher-Yates' algorithm will be used.
@@ -15,10 +15,6 @@ export function shuffle<T>(arr: T[]): T[] {
   return arr;
 }
 
-export function moveFromStack<T>(source: T[], target: T[], n: number) {
-  target.push(...source.splice(-n).reverse());
-}
-
 export function dealCards(players: IPlayer[], deck: mongoose.Types.ObjectId[]) {
   for (const player of players) {
     player.hand.push(...deck.splice(0, 7));
@@ -26,7 +22,7 @@ export function dealCards(players: IPlayer[], deck: mongoose.Types.ObjectId[]) {
 }
 
 export async function getFirstNonWild(deck: mongoose.Types.ObjectId[]) {
-  const promises = deck.map(async (c) => await Card.findById(c));
+  const promises = deck.map((c) => Card.findById(c));
   const results = await Promise.all(promises);
   const index = results.findIndex((dbCard) => {
     return dbCard?.color !== CardColor.wild;
